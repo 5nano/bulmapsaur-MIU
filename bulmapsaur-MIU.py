b523a1f09703bc9f376b12d839c23b93f485d599
lucasAmoroso <lucas.amoroso@despegar.com>
2019-11-15 11:13:05 -0300

    code refactor now experiments by day

--- bulmapsaur-MIU.py
+++ bulmapsaur-MIU.py
@@ -13,19 +13,12 @@ parser.add_argument('-u','--url', type=str, required=True,
                    help='Url to post data')
 parser.add_argument('-s','--sec', type=int,required=True,
                    help='Seconds to wait between images to send')
-parser.add_argument('-ia','--idAssay', type=str,required=True,
-                   help='Assay identifier')
-parser.add_argument('-ie','--idExperiment', type=str, required=True,
-                   help='Experiment identifier identifier')     
-parser.add_argument('-date', '--optionalDate', type=str, required=False,
-                    help='Photo date in format "2018-06-29 08:15:27.243860"')
+
 args = vars(parser.parse_args())
 images_directory = args["dir"]
 destination_url = args["url"]
 wait_seconds = args["sec"]
-id_assay = args["idAssay"]
-id_experiment = args["idExperiment"]
-opt_date = args["optionalDate"]
+
 
 ENCODING = 'utf-8'
 
@@ -35,39 +28,40 @@ def get_files_sorted_by_modified(dirpath):
     a = [s for s in os.listdir(dirpath)
          if os.path.isfile(os.path.join(dirpath, s))]
     a.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)))
-    return a
-
-def build_body() :
-    if opt_date != None: 
-        return  {
-                  'idAssay': id_assay,
-                  'idExperiment': id_experiment,
-                   'base64': base64_string,
-                   "optionalTime": opt_date
-                }
-    else :
-        return {
-                'idAssay': id_assay,
-                'idExperiment': id_experiment,
-                'base64': base64_string              
-                }                    
+    return a                
 
 
 for image_name in get_files_sorted_by_modified(images_directory):
 
        with open(os.path.join(images_directory,image_name), "rb") as image:
-
+            
             print('Image name:', image_name)
+
+            full_path = images_directory.split("/")
+            date_position = len(full_path) - 1
+            date = full_path[date_position]
+            idAssay = full_path[date_position -1 ]
+            
+            idExperiment = image_name.split(".jpg")[0]
+            print("Date directory " + date)
+            print("IdAssay " + idAssay)
+            print('Id experiment:', idExperiment)
            
             print('Building b64 ...')
             base64_bytes = b64encode(image.read())
             base64_string = base64_bytes.decode(ENCODING)
             
             print('Requesting to', destination_url)
+            body = {
+                  'idAssay': idAssay,
+                  'idExperiment': idExperiment,
+                  'base64': base64_string,
+                  'optionalTime': date + " " + "08:15:27.243860"
+                }
             try:
                 resp = requests.post(destination_url, 
-                        json=build_body(),
-                        headers=headers)
+                        json=body,
+                       headers=headers)
                 print(resp)
             except Exception as e:
                 print(e)
